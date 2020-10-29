b8e368ebb4ff13e053833000202f48fe9d25ecba
Todimu Jenrola <todimujenrola@gmail.com>
2020-10-29 08:54:10 -0400

    Commit

---
+++ Queue.py
@@ -0,0 +1,34 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enque(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if len(self.items) < 1:
+            return None
+        return self.items.pop()
+
+    def print_stack(self):
+        print(self.items)
+
+    def size(self):
+        return len(self.items)
+
+q = Queue()
+q.enque(2)
+q.enque(1)
+q.enque(4)
+q.enque(45)
+q.enque(76)
+q.enque(30)
+print(q.size())
+q.print_stack()
+q.dequeue()
+print(q.size())
+q.print_stack()
+
--- Stack.py
+++ Stack.py
@@ -14,18 +14,9 @@ class Stack:
     def print_stack(self):
         print(self.items)
 
-class Queue:
-    def __init__(self):
-        self.items = []
+    def size(self):
+        return len(self.items)
 
-    def isEmpty(self):
-        return self.items == []
-
-    def enque(self, item):
-        self.items.append(item)
-
-    def dequeue(self):
-        return self.items.pop()
 
 
 s = Stack()
@@ -37,4 +28,5 @@ s.push(276)
 s.print_stack()
 s.pop()
 s.print_stack()
+print(s.size())
     
