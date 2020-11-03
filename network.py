fb1a2af4e617f7fdc9bc39a89b9a8e668bea5eb2
Todimu Jenrola <todimujenrola@gmail.com>
2020-11-02 22:14:22 -0500

    Network

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import psutil
+import speedtest
 from tabulate import tabulate
 
 class Network_Details(object):
@@ -27,5 +28,33 @@ class Network_Details(object):
         
         return tabulate(data, headers="keys", tablefmt="github")
 
+
+class Speed_Test(object):
+    def __init__(self):
+        self.parser = psutil.net_if_addrs()
+        self.speed_parser = speedtest.Speedtest()
+        self.interfaces = self.interfaces()[0]
+
+    def interface(self):
+        interfaces =[]
+        for interface_name, _ in self.parser.items():
+            interfaces.append(str(interface_name))
+        return interfaces
+
+    def __repr__(self):
+        down = str(f"{round(self.speed_parser.download() / 1_000_000, 2)} Mbps")
+        up = str(f"{round(self.speed_parser.upload() /1_000_000, 2)} Mbps")
+        interface = self.interfaces
+
+        data = {"Interface"        :[interface],
+                "Download"       :[down], 
+                "Upload"          :[up]
+            }
+        
+        return tabulate(data, headers="keys", tablefmt="pretty")
+    
+
+
+
 if __name__ == "__main__":
-    print(Network_Details())
+    print(Speed_Test())
