b43b43e269bcba67c3ccd6f3a4a435192b615302
Todimu Jenrola <todimujenrola@gmail.com>
2020-11-08 11:46:43 -0500

    Convert text file from website to PDF

---
+++ downloader.py
@@ -0,0 +1,45 @@
+from urllib import request
+from datetime import date, datetime, timedelta
+
+sites = ['']
+
+index = 0
+
+def multiple():
+    for site in sites:
+        response = request.urlopen(site)
+        txt = response.read()
+        txt_str = str(txt)
+        lines = txt_str.split("\\n")
+        today = datetime.now().strftime("%Y-%m-%d")
+        destination = r'"File Path"' + '{}'.format(today) + '__({})'.format(index) + '.txt'
+        fx = open(destination, "w")
+        for line in lines:
+            fx.write(line + "\n")
+        fx.close()
+        index += 1
+
+
+
+   
+def single(website):
+    response = request.urlopen(website)
+    txt = response.read()
+    txt_str = str(txt)
+    lines = txt_str.split("\\n")
+    today = datetime.now().strftime("%Y-%m-%d")
+    destination = r'"File Path' + '{}.txt'.format(today)
+    fx = open(destination, "w")
+    for line in lines:
+        fx.write(line + "\n")
+    fx.close()
+
+
+#single(sites[2])
+#multiple()
+single(website)
+
+
+
+
+
