23ccf4518e6d00421ab707470ddb4f17e9044d47
Todimu Jenrola <todimujenrola@gmail.com>
2021-01-25 21:30:40 -0500

    Binary Search Algorithm

---
+++ binarySearch.py
@@ -0,0 +1,9 @@
+def binarySearch(array, number):
+    bIndex = 0
+    eIndex = len(array) -1 
+
+    while bIndex <= eIndex:
+        midpoint = bIndex + (eIndex - bIndex) // 2
+        midpoint_value = array[midpoint]
+
+        if 
