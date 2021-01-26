dd1bc889a4d9b035778fb7f84e29136235274c22
Todimu Jenrola <todimujenrola@gmail.com>
2021-01-25 22:10:20 -0500

    Binary Search Algorithm

--- binarySearch.py
+++ binarySearch.py
@@ -1,27 +1,27 @@
-def binarySearch(array, number):
+def binarySearch(array, target):
     bIndex = 0
-    eIndex = len(array) - 1 
+    eIndex = len(array) - 1
 
     while bIndex <= eIndex:
-        midpoint = bIndex + (eIndex - bIndex) // 2
+        midpoint = (bIndex + eIndex) // 2
         midpoint_value = array[midpoint]
 
-        if midpoint_value == number:
+        if midpoint_value == target:
             return midpoint
         
-        elif number < midpoint_value:
+        elif target < midpoint_value:
             eIndex = midpoint - 1
         
-        elif number > midpoint_value:
+        elif target > midpoint_value:
             bIndex = midpoint + 1
 
     return None
 
 sequence = [2, 4, 1, 7, 8, 3, 96, 340, 76, 24, 21, 11, 9, 54, 32, 54, 67, 33]
 item1 = 2
-item2 = 76
+item2 = 7
 item3 = 24
-item4 = 37
+item4 = 96
 
 print(binarySearch(sequence, item1))
 print(binarySearch(sequence, item2))
