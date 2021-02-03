bd90cf8748ce408082c52b4a91e56b9b8b4088db
Todimu Jenrola <todimujenrola@gmail.com>
2021-02-03 16:17:27 -0500

    Trie data structure for auto-completion

--- trie.py
+++ trie.py
@@ -4,28 +4,28 @@ class Node:
         self.isWord = isWord
 
 class Solution:
-    def __init__(self)):
+    def __init__(self):
         self.trie = None
 
     def build(self, words):
-        self.trie = Node(False, {})
+        self.trie = Node({}, False)
         for word in words:
             current = self.trie
             for char in word:
                 if not char in current.children:
-                     current.children[char] = Node(False, {})
+                     current.children[char] = Node({}, False)
                 current = current.children[char]
             current.isWord = True 
                  
     
     def autocomplete(self, prefix):
         current = self.trie
-        for char in word:
+        for char in prefix:
             if not char in current.children:
                 return []
             current = current.children[char]
         
-        return self.findWordsFromNode(current, word)
+        return self.findWordsFromNode(current, prefix)
 
     def findWordsFromNode(self, node, prefix):
         words = []
@@ -40,7 +40,7 @@ class Solution:
     
 
 s = Solution()
-s.build(['dog', 'dark', 'cat', 'door', 'dodge', 'dentist', 'fume'])
+s.build(['dog', 'dark', 'cat', 'door', 'dodge', 'dentist', 'fume', 'doodle'])
 print(s.autocomplete('do'))
 # ['dog', 'door', 'dodge']
 
