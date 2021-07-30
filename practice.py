caf4420f45faacddbfb81474fd02780a8b9dbe00
Todimu Jenrola <todimujenrola@gmail.com>
2021-07-29 22:56:35 -0400

    Practice

--- practice.py
+++ practice.py
@@ -144,4 +144,4 @@
 ####################################################################### K Closest points to origin
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]: 
-        return sorted(points, key = lambda x: x[0]**2 + x[1])
+        return sorted(points, key = lambda x: x[0]**2 + x[1]**2)
