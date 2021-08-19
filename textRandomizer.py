a4e37b9877fe69780af3c042f4dc27832ae47dc1
Todimu Jenrola <todimujenrola@gmail.com>
2021-08-18 21:41:53 -0400

    Random text generator

--- textRandomizer.py
+++ textRandomizer.py
@@ -15,13 +15,7 @@ f = open("textRandom.txt","w+")
 for i in range(10000):
     if i%30 == 0:
         f.write("\n")
-        
+
     f.write(random.choice(words) + " ")
 
 f.close()
-
-# with open ('myfile', 'a') as f: 
-#     for i in range(1000):
-#         f.write (random.choice(words) + " ")
-
-# print(words)
