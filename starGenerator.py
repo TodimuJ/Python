3d7af722fe063ce7278fea7d5ba4f7dbe09874d4
Todimu Jenrola <todimujenrola@gmail.com>
2021-09-19 20:05:43 -0400

    Generate a star of length n

--- starGenerator.py
+++ starGenerator.py
@@ -1,8 +1,8 @@
-number = int(input("What is n?: "))
+number = int(input("What is n: "))
 
 for i in range(number):
     for j in range(number):
-        if i == j or j == (i-number-1):
+        if i == j or j == (number-i-1):
             print("*")
         
         else:
