d7285ff572ce32d73a49bb37a6aaaa536435219e
Todimu Jenrola <todimujenrola@gmail.com>
2021-10-20 11:01:21 -0400

    Make gym reservation

---
+++ gymReservation.py
@@ -0,0 +1,10 @@
+import pyscreenshot as screen
+import time, os, math, re, webbrowser, glob, random, shutil, img2pdf
+import urllib.request 
+import selenium.webdriver.chrome.webdriver
+from selenium.webdriver import ActionChains
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+url = 'https://reservation.frontdesksuite.ca/rcfs/richcraftkanata'
