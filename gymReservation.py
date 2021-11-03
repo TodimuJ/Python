8e4cb600c488e8173faf40c85b2cb5928ff38a71
Todimu Jenrola <todimujenrola@gmail.com>
2021-11-02 22:48:54 -0400

    Simple countdown timer 2

--- gymReservation.py
+++ gymReservation.py
@@ -1,13 +1,16 @@
 import gymReservationDetails as grd
-import time, re, webbrowser
+import time, os, re, webbrowser
 import urllib.request 
-import selenium.webdriver.chrome.webdriver
-from selenium.webdriver import ActionChains
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+# # # from webdriver_manager.chrome import ChromeDriverManager
+# import selenium.webdriver.chrome.webdriver
+# from selenium.webdriver import ActionChains
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.chrome.options import Options
 
 url = 'https://reservation.frontdesksuite.ca/rcfs/richcraftkanata'
 
 driver = webdriver.Chrome()
-driver.get(url)
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver.get(url)
+print("test")
