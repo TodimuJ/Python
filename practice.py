57d915586225d2d6ba81eb81a8dc642009ba8429
TodimuJ <todimujenrola@gmail.com>
2022-04-20 22:10:40 -0400

    Practice

--- practice.py
+++ practice.py
@@ -193,3 +193,11 @@
 
 ####################################################################### Is same tree
 def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        
+        if not p or not q or (p.val != q.val):
+            return False
+        
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) 
