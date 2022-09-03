c5d478be2e5e3e7846c18197ba3239c6d71a8ae8
TodimuJ <todimujenrola@gmail.com>
2022-09-02 20:13:30 -0400

    QR code generator

---
+++ qrCode.py
@@ -0,0 +1,10 @@
+import pyqrcode
+from PIL import Image
+
+link = input("Enter anything to generate QR: ")
+qr_code = pyqrcode.create(link)
+qr_code.png("QRCode.png", scale=5)
+Image.open("QRCode.png")
+
+print("Enter anything to generate QR:")
+# https://twitter.com/clcoding
