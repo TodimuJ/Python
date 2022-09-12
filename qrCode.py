5f6ccf8e7f644aa27ae746d31e174553ba65e16d
TodimuJ <todimujenrola@gmail.com>
2022-09-11 21:21:18 -0400

    QR code generator

--- qrCode.py
+++ qrCode.py
@@ -6,4 +6,11 @@ qr_code = pyqrcode.create(link)
 qr_code.png("QRCode.png", scale=5)
 Image.open("QRCode.png")
 
-print("Enter anything to generate QR:")
+while True:
+    query = input("Type something to put in QR: ")
+    
+    print(query)
+    if query == " ":
+        break
+
+    print("Enter anything to generate QR: \n")
