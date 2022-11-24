379f0a84abdf36f516d4f92b0ac6e9f7ead2d8f5
TodimuJ <todimujenrola@gmail.com>
2022-11-24 17:45:12 -0500

    Bar chart using matplotlib

---
+++ barChart.py
@@ -0,0 +1,11 @@
+import matplotlib as plt
+
+def bar_chart(members, labels, pos):
+    plt.bar(pos, numbers, color='blue')
+    plt.xticks(ticks=pos, labels=labels)
+    plt.show()
+
+if __name__ == '__main__':
+    numbers = [2, 1, 4, 6]
+    labels = ['Electric', 'Solar', 'Diesel', 'Unleaded']
+    
